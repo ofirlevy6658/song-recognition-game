b761026b806f96980d19c202ffd00053ba0fa62e
ofirlevy6658 <ofirlevy6658@gmail.com>
2021-05-10 13:27:32 +0300

    heroku

--- client/src/API/api.js
+++ client/src/API/api.js
@@ -1,7 +1,14 @@
 import axios from "axios";
+let origin;
+if (process.env.NODE_ENV === "development") {
+	origin = "http://localhost:4000/api";
+}
+if (process.env.NODE_ENV === "production") {
+	origin = "/api";
+}
 
 const api = axios.create({
-	baseURL: "http://localhost:4000/api",
+	baseURL: origin,
 });
 
 export default api;
--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 // const axios = require("axios");
+// const Song = require("./models/song");
 const cors = require("cors");
 const songRouter = require("./routers/song");
 const userRouter = require("./routers/user");
-// const Song = require("./models/song");
+const path = require("path");
 require("./db/mongoose");
-
 const app = express();
+
+const publicDirectory = path.join(__dirname, "../client/build");
+app.use(express.static(publicDirectory));
 app.use(cors());
 app.use(express.json());
 app.use(userRouter);
